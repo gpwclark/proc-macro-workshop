b72fe03ad51f7c22bc2b70b0c257bfa2ef83e66e
gpwclark <gpwclark@gmail.com>
2022-05-26 04:32:34 +0000

    do it properly

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,7 +1,38 @@
 use proc_macro::TokenStream;
-use quote::{quote};
-use syn::{parse_macro_input, DeriveInput, Ident, Data, Fields, Field};
+use quote::{quote, quote_spanned};
+use syn::{parse_macro_input, DeriveInput, Ident, Data, Fields};
+use syn::spanned::Spanned;
 use syn::__private::{Span, TokenStream2};
+
+fn get_builder_impl(data: &Data) -> TokenStream2 {
+    match *data {
+        Data::Struct(ref data) => {
+            match data.fields {
+                Fields::Named(ref fields) => {
+                    let impls = fields.named.iter().map(|f| {
+                        let name = &f.ident;
+                        let ty = &f.ty;
+                        quote_spanned! {f.span()=>
+                            fn #name(&mut self, #name: #ty) -> &mut Self {
+                                self.#name = Some(#name);
+                                self
+                            }
+                            
+                        }
+                    });
+                    quote! {
+                       #(#impls
+                        )*
+                   }
+                }
+                Fields::Unnamed(_) => { unimplemented!() }
+                Fields::Unit => { unimplemented!() }
+            }
+        }
+        Data::Enum(_) => { unimplemented!()}
+        Data::Union(_) => { unimplemented!()}
+    }
+}
 
 fn get_builder_definition(data: &Data) -> TokenStream2 {
     match *data {
@@ -53,6 +84,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let empty_builder = get_empty_builder(&input.data);
     let builder_defn = get_builder_definition(&input.data);
+    let builder_impl = get_builder_impl(&input.data);
     let name = input.ident;
     let builder = format!("{}Builder", name);
     let builder_name = Ident::new(&builder, Span::call_site());
@@ -70,25 +102,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
         
         impl #builder_name {
-            fn args(&mut self, args: Vec<String>) -> &mut Self {
-                self.args = Some(args);
-                self
-            }
-            
-            fn env(&mut self, env: Vec<String>) -> &mut Self {
-                self.env = Some(env);
-                self
-            }
-            
-            fn current_dir(&mut self, current_dir: String) -> &mut Self {
-                self.current_dir = Some(current_dir);
-                self
-            }
-            
-            fn executable(&mut self, executable: String) -> &mut Self {
-                self.executable = Some(executable);
-                self
-            }
+            #builder_impl
         }
     };
     TokenStream::from(tokens)
