a1382770188196dd7bfed0fdd74d6180159bf513
gpwclark <gpwclark@gmail.com>
2022-05-26 04:01:54 +0000

    do it properly

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,11 +1,27 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, DeriveInput, Ident};
+use quote::{quote};
+use syn::{parse_macro_input, DeriveInput, Ident, Data, Fields};
 use syn::__private::Span;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
+    let field_names = match input.data {
+        Data::Struct(ref data) => {
+            match data.fields {
+                Fields::Named(ref fields) => {
+                    fields.named.iter().map(|f| {
+                        let name = &f.ident;
+                        quote! { #name }
+                    })
+                }
+                Fields::Unnamed(_) => { unimplemented!() }
+                Fields::Unit => { unimplemented!() }
+            }
+        }
+        Data::Enum(_) => { unimplemented!()}
+        Data::Union(_) => { unimplemented!()}
+    };
     let name = input.ident;
     let builder = format!("{}Builder", name);
     let builder_name = Ident::new(&builder, Span::call_site());
@@ -13,10 +29,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
         impl #name {
             pub fn builder() -> #builder_name {
                 #builder_name {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
+                    #(#field_names: None,)*
                 }
             }
         }
@@ -28,6 +41,27 @@ pub fn derive(input: TokenStream) -> TokenStream {
             current_dir: Option<String>,
         }
         
+        impl #builder_name {
+            fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+            
+            fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+            
+            fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
+            }
+            
+            fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+        }
     };
     TokenStream::from(tokens)
 }
