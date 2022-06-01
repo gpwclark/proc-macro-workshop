35bf2fbd809685a6c4248d2ce2e7f6a55de0af80
gpwclark <gpwclark@gmail.com>
2022-06-01 05:40:33 +0000

    freebie

--- main.rs
+++ main.rs
@@ -24,6 +24,13 @@ fn main() {
     builder.env(vec![]);
     builder.current_dir("..".to_owned());
 
-    let command = builder.build().unwrap();
+    let command = Command::builder()
+        .executable("cargo".to_owned())
+        .args(vec!["build".to_owned(), "--release".to_owned()])
+        .env(vec![])
+        .current_dir("..".to_owned())
+        .build()
+        .unwrap();
+
     assert_eq!(command.executable, "cargo");
 }
